05f81bdf7373ced1bc2c4cb14c4dcd8eaccdeb73
alessiam1909 <ale.menon.vr@gmail.com>
2023-03-07 18:23:09 +0100

    popolazione della tabella types con seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Str;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,15 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = ['Laravel', 'Php', 'Javascript', 'CSS', 'HTML'];
+
+        foreach($types as $type){
+            $newType = new Type();
+
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name, '-');
+
+            $newType->save();
+        }
     }
 }
